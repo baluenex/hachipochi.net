fecb143323a61f659f74507b539e9e69dbd9160f
Unknown <baluenex@gmail.com>
2019-02-25 09:19:31 +0900

    [update]css調整、vue.jsによるjson取得、galleryページの改装

--- js/gallery.js
+++ js/gallery.js
@@ -2,7 +2,21 @@ window.onload = function(){
   var sitenm = new Vue({
     el: '#sitenm',
     data: {
-      message: 'はちぽち.net'
+      message: 'はちぽちの部屋'
     }
   })
+
+  const vm = new Vue({
+    el: '#img',
+    data: {
+      results: [
+        {
+          "img_id":"hachipochi_hakusai.JPG_img",
+          "img_name":"https://s3-ap-northeast-1.amazonaws.com/hachipochi-img/hachipochi_hakusai.JPG",
+          "img_type":"hachipochi",
+          "title":"はちぽちと白菜"
+        }
+      ]
+    }
+  });
 }
