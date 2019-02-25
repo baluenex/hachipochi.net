6b84c67d543e7a2d4a38fcbdaa9b9a5b471d961e
Unknown <baluenex@gmail.com>
2019-02-25 18:05:02 +0900

    [update]vue.jsでAPIをたたいてjsonを取得し、画像タイトルと画像を描写

--- js/gallery.js
+++ js/gallery.js
@@ -1,22 +1,15 @@
 window.onload = function(){
-  var sitenm = new Vue({
-    el: '#sitenm',
-    data: {
-      message: 'はちぽちの部屋'
-    }
-  })
-
   const vm = new Vue({
     el: '#img',
-    data: {
-      results: [
-        {
-          "img_id":"hachipochi_hakusai.JPG_img",
-          "img_name":"https://s3-ap-northeast-1.amazonaws.com/hachipochi-img/hachipochi_hakusai.JPG",
-          "img_type":"hachipochi",
-          "title":"はちぽちと白菜"
-        }
-      ]
+    data() {
+      return {
+        data: null
+      }
+    },
+    mounted() {
+      axios
+        .get('https://atps3sxmb9.execute-api.ap-northeast-1.amazonaws.com/default/get_gallery_img')
+        .then(response => (this.data = response.data))
     }
   });
 }
