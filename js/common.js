8d6a76a45ab1835f10b14b703021c409cac81782
Unknown <baluenex@gmail.com>
2019-03-16 01:40:02 +0900

    [change]ファイルの構成を変更

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ function head(){
   var head = "";
   head += '<title>はちぽち.net</title>';
   head += '<link rel="shortcut icon" href="https://s3-ap-northeast-1.amazonaws.com/hachipochi-img/dog_nikukyu.png" type="image/">';
-  head += '<link rel="stylesheet" type="text/css" href="../css/common.css">';
+  head += '<link rel="stylesheet" type="text/css" href="./css/common.css">';
   head += '<link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,700" rel="stylesheet">';
   head += '<meta name="description" content="僕たちはちぽちをはじめとする、とても可愛らしい我が家のアニマルグッズを紹介するサイトだわん！わん！">'
   document.write(head);
